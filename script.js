ab8f9c8e15f6531972a853ff7edaac89ab275ef0
Gui <168383419+Guilherme220501@users.noreply.github.com>
2024-10-04 14:40:01 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ mobileMenu.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
 function showAlert(message) {window.onload = function () {alert(message);};}
+// Função para abrir e fechar o menu lateral
+function toggleMenu() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.style.left === '-250px') {
+        sidebar.style.left = '0'; // Abre o menu
+    } else {
+        sidebar.style.left = '-250px'; // Fecha o menu
+    }
+}
